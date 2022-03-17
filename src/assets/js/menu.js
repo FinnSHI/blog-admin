938fe81055f51894acaa2a8ce962edeb8398b2b7
finn <shifanfinn@163.com>
2022-03-17 14:26:06 +0800

    init

---
+++ src/assets/js/menu.js
@@ -0,0 +1,50 @@
+import Layout from "@/layout/index.vue";
+import router from "../../router";
+import store from "../../store";
+import axios from "axios";
+import Vue from "vue";
+
+export function generaMenu() {
+  // 查询用户菜单
+  axios.get("/api/admin/getMenuList").then(({ data }) => {
+    if (data.status) {
+      var userMenuList = data.data.menuList;
+      userMenuList.forEach(item => {
+        if (item.icon != null) {
+          item.icon = "iconfont " + item.icon;
+        }
+        if (item.component == "Layout") {
+          item.component = Layout;
+        }
+        if(item.name == "首页") {
+          item.component = Layout;
+          // route => {
+          //   route.icon = "iconfont " + route.icon;
+          //   route.component = loadView(route.component);
+          //   console.log(route.component)
+          // };
+        }
+        if (item.children && item.children.length > 0) {
+          item.children.forEach(route => {
+            route.icon = "iconfont " + route.icon;
+            route.component = loadView(route.component);
+          });
+        } 
+      });
+      console.log(userMenuList);
+      // 添加侧边栏菜单
+      store.commit("saveUserMenuList", userMenuList);
+      // 添加菜单到路由
+      router.addRoutes(userMenuList);
+    }
+    else {
+      Vue.prototype.$message.error(data.message);
+      router.push({ path: "/login" });
+    }
+  });
+}
+
+export const loadView = view => {
+  // 路由懒加载
+  return resolve => require([`@/views${view}`], resolve);
+};
